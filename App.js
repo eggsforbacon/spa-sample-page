4938cc96e6724c35a2492602732ce0da40bcd4e9
JunaiCode <juanpalta24@hotmail.com>
2022-02-13 17:44:32 -0500

    fix: parseRequest error

--- App.js
+++ App.js
@@ -10,9 +10,9 @@ let parseRequestURL = () => {
         verb: null
     }
 
-    request[resource] = r[1];
-    request[id] = r[2];
-    request[verb] = r[3];
+    request.resource = r[1];
+    request.id = r[2];
+    request.verb = r[3];
 
     return request;
 }
