144ea2c23f4bee2832edaa8a37b858b16725fe86
JunaiCode <juanpalta24@hotmail.com>
2022-02-13 12:46:30 -0500

    feat: add listener,and get the url

--- app/js/script.js
+++ app/js/script.js
@@ -10,6 +10,22 @@ const routes = {
 const router = async () => {
     //Lazy load
     const content = null || document.getElementById('page_container');
-
+    // get the url with the split '/'
     let request = Utils.parseRequestURL();
+
+    // Parse the URL - we fill the url according to split
+    let parsedURL = (request.resource ? '/' + request.resource : '/') + (request.id ? '/:id' : '') + (request.verb ? '/' + request.verb : '')
+
+    //With the parsedURL we ask if the route exist, if no exist launch 404 page
+    let page = routes[parsedURL] ? routes[parsedURL] : Error404;
+
+    // await is to manipulate sync
+    content.innerHTML = await page.render();
+    await page.after_render();
 }
+
+// Listen on hash change #, we pass the function to launch
+window.addEventListener('hashchange',router);
+
+//Liste load
+window.addEventListener('load',router);
