e3d63ea905ea6c339cd9d038f0cf678c82379f9f
JunaiCode <juanpalta24@hotmail.com>
2022-02-13 16:51:13 -0500

    fix: only file, correctly order

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-parseRequestURL : () => {
+let parseRequestURL = () => {
     let url = location.hash.slice(1).toLowerCase() || '/';
     let r = url.split("/");
 
@@ -14,62 +14,10 @@ parseRequestURL : () => {
 
     return request;
 }
-/* Routing */
-
-const routes = {
-    '/': 'Home',
-    '/about': 'About',
-    '/p/:id': 'PostShow',
-    '/register': 'Register'
-};
-
-const router = async () => {
-    //Lazy load view elemtents
-    const content = null || document.getElementById('page__container');
-    const header = null || document.getElementById('header__container');
-    const footer = null || document.getElementById('footer__container');
-
-    //Render components in page (header and footer) <- no matter what url is requested, header and footer always render
-    header.innerHTML = await Header.render();
-    await Header.after_render();
-    footer.innerHTML = await Footer.render();
-    await Footer.after_render();
-
-    // get the url with the split '/'
-    let request = Utils.parseRequestURL();
-
-    // Parse the URL - we fill the url according to split
-    let parsedURL = (request.resource ? '/' + request.resource : '/') + (request.id ? '/:id' : '') + (request.verb ? '/' + request.verb : '')
-
-    //With the parsedURL we ask if the route exist, if no exist launch 404 page
-    let page = routes[parsedURL] ? routes[parsedURL] : Error404;
-
-    // await is to manipulate sync
-    content.innerHTML = await page.render();
-    await page.after_render();   
-}
 
 let OnRegister = () => {
     console.log("ayo");
     alert("ayo");
-}
-
-let About = {
-    render : async () => {
-        let view = /*html*/`
-            <section class="about__hero">
-                <h1>About</h1>
-                <p>This is the about page</p>
-                <button id ="header__btn-register">Button</button>
-            </section>
-        `
-
-        return view;
-    },
-
-    after_render : async () => {
-        document.getElementById('header__btn-register').addEventListener("click", OnRegister());
-    }
 }
 
 let Header = {
@@ -103,6 +51,59 @@ let Footer = {
 
     after_render : async () => {/* Put events in here */}
 }
+
+let About = {
+    render : async () => {
+        let view = /*html*/`
+            <section class="section">
+                <h1>About</h1>
+                <p>This is the about page</p>
+                <button id ="header__btn-register">Button</button>
+            </section>
+        `
+
+        return view;
+    },
+
+    after_render : async () => {
+        document.getElementById('header__btn-register').addEventListener("click", OnRegister());
+    }
+}
+
+/* Routing */
+const routes = {
+    // '/': Home,
+      '/about': About,
+     //'/p/:id': PostShow,
+     //'/register': Register
+ };
+
+ const router = async () => {
+    //Lazy load view elemtents
+    const content = null || document.getElementById('page__container');
+    const header = null || document.getElementById('header__container');
+    const footer = null || document.getElementById('footer__container');
+
+    //Render components in page (header and footer) <- no matter what url is requested, header and footer always render
+    header.innerHTML = await Header.render();
+    await Header.after_render();
+    footer.innerHTML = await Footer.render();
+    await Footer.after_render();
+
+    // get the url with the split '/'
+    let request = parseRequestURL();
+
+    // Parse the URL - we fill the url according to split
+    let parsedURL = (request.resource ? '/' + request.resource : '/') + (request.id ? '/:id' : '') + (request.verb ? '/' + request.verb : '')
+
+    //With the parsedURL we ask if the route exist, if no exist launch 404 page
+    let page = routes[parsedURL] ? routes[parsedURL] : Error404;
+
+    // await is to manipulate sync
+    content.innerHTML = await page.render();
+    await page.after_render();
+}
+
 // Listen on hash change #, we pass the function to launch
 window.addEventListener('hashchange',router);
 
