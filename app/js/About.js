0c2047f8cecc6c219e8ad34763e9ef390b3fed3b
zacwastaken <doicirohangnan@gmail.com>
2022-02-13 13:07:04 -0500

    save state

---
+++ app/js/About.js
@@ -0,0 +1,14 @@
+let About = {
+    render : async () => {
+        let view = /*html*/`
+            <section class="about__hero">
+                <h1>About</h1>
+                <p>This is the about page</p>
+            </section>
+        `
+
+        return view;
+    }
+}
+
+export default About;
